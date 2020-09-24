b4b0adb9d1cb6e5fe67bdd1f67ff3dd49a3cc5be
southxz <southxzx109@users.noreply.github.com>
2020-09-24 10:11:26 +0700

    ok

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useRef, useState } from 'react';
 
 function TodoForm(props) {
 
     const [input, setInput] = useState('');
 
+    const inpuRef = useRef(null);
+
+    useEffect(() => {
+        inpuRef.current.focus();
+    }, [input])
+
     const handleSubmit = (e) => {
         // Chặn submit của button
         e.preventDefault();
 
+        // Object
         props.onSubmit({
             id: Math.floor(Math.random() * 10000),
             text: input
@@ -29,7 +35,8 @@ function TodoForm(props) {
                 value={input} 
                 name="text" 
                 className="todo_input" 
-                onChange={handleChange}/>
+                onChange={handleChange}
+                ref={inpuRef}/>
             <button className="todo_button">Add</button>
         </form>
     )
